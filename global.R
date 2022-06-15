0ab36244ad52dfb514259463f80c880cc4a47834
augustocfg <augustocesarfg@gmail.com>
2022-06-15 17:03:55 -0300

    boot inicial

---
+++ global.R
@@ -0,0 +1,7 @@
+library(shiny)
+library(tidyverse)
+library(argonDash)
+library(echarts4r)
+        
+source("ui.R")
+source("server.R")
---
+++ ui.R
@@ -0,0 +1,11 @@
+ui <- argonDash::argonDashPage(
+  title = "Dash Contas Públicas",
+  author = "Augusto Gurgel @augustocfg",
+  description = "lorum",
+  navbar = argonDashNavbar(
+    argonDropNav(title = "Dash Contas Públicas",
+                 orientation = "left",
+                 src = "icon.jpg")
+  )
+)
+  
